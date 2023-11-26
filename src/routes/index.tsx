1b8f07bf25185ad200b15c6e406e629eb55ca04c
MohammadReza <md.mohammadreza+git@gmail.com>
2023-11-26 13:41:38 +0330

    remove log

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,12 +3,8 @@ import Navbar from "@components/navbar";
 import AddLocation from "./addLocation";
 import MapLocation from "./map";
 import EditLocation from "./editLocation";
-import useLocationStore from "@store/location";
 
 export default function Index() {
-  const { locationList } = useLocationStore();
-  console.log("🚀 ~ file: index.tsx:11 ~ Index ~ locationList:", locationList);
-
   const router = createBrowserRouter([
     {
       element: <Navbar />,
